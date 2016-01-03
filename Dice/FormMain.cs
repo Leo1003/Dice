13da1b8612867b06f4241751524340714d16f597
Leo Chen <leo881003@gmail.com>
2016-01-03 10:39:00 +0800

    亂數初始更新

--- Dice/FormMain.cs
+++ Dice/FormMain.cs
@@ -5,14 +5,15 @@ namespace Dice
 {
     public partial class FormMain : Form
     {
+        Random r;
         public FormMain()
         {
             InitializeComponent();
+            r = new Random((int)DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Random r = new Random(Convert.ToInt32(DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds % int.MaxValue));
             textBox1.Text = r.Next(1, 7).ToString();
         }
     }
